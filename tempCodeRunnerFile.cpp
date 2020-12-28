8b948761de4bd3c91f0cf803da8ad775a94e0263
ap <adeelparvez33@gmail.com>
2020-12-28 23:14:17 +0530

    first commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// #include <string>
+using namespace std;
+
+int main() {
+  int i = 0,n;
+  cout << "Wnter Tesxr!!";
+  cin >> n;
+  while (i <= n) {
+    cout << i << endl;
+    i+=5;
+
+  }
+  return 0;
---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+    int i;
+    printf("Enter value_  ");
+    scanf("%d",&i);
+    while (i>=0)
+    {   
+        printf("%d : Run Successsful\n",i);
+        i--;
+    }
+    
+}
